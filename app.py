4cf2a567092b0326a59e5678c3545794b4f8186b
sam1202tcb <mas7437@gmail.com>
2023-08-11 14:08:01 +0200

    added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from appp import BOSS_S_K
 import os
 
 load_dotenv()
@@ -6,6 +7,8 @@ load_dotenv()
 # loading every .env file in the project
 api_key = os.environ.get('MY_API_KEY')
 sk = os.getenv('SECRET_KEY')
+B_S_K = BOSS_S_K
 print(api_key)
+print(B_S_K)
 print(sk)
 
